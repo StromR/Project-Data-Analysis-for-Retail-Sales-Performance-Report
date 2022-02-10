b44f8fae360fac328d881d3bee4905b0589ff111
Guntur Ramadhan <gunturramadhan25@gmail.com>
2022-02-10 08:17:31 +0700

    Update DQLAB Store Performance_Queries.sql

--- DQLAB Store Performance_Queries.sql
+++ DQLAB Store Performance_Queries.sql
@@ -145,3 +145,22 @@ ORDER BY
 */
 
 
+-- Customers Transactions per Year
+
+SELECT
+	YEAR(order_date) AS years,
+	COUNT(DISTINCT(customer)) AS number_of_customer
+FROM dqlab_sales_store
+WHERE order_status = 'Order Finished'
+GROUP BY years;
+
+/* Output:
++-------+--------------------+
+| years | number_of_customer |
++-------+--------------------+
+|  2009 |                585 |
+|  2010 |                593 |
+|  2011 |                581 |
+|  2012 |                594 |
++-------+--------------------+
+*/
