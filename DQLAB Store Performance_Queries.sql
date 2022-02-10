fba5ab36f236d2e9695583147d3365e6379a5e96
Guntur Ramadhan <gunturramadhan25@gmail.com>
2022-02-10 07:23:45 +0700

    Update DQLAB Store Performance_Queries.sql

--- DQLAB Store Performance_Queries.sql
+++ DQLAB Store Performance_Queries.sql
@@ -8,7 +8,7 @@ FROM dqlab_sales_store
 WHERE order_status = 'Order Finished'
 GROUP BY years;
 
-output:
+-- output:
 +-------+------------+-----------------+
 | years | sales      | number_of_order |
 +-------+------------+-----------------+
@@ -37,8 +37,7 @@ ORDER BY
 	years ASC,
 	sales DESC;
 	
-
-output:
+-- output:
 +-------+--------------------------------+-----------+
 | years | product_sub_category           | sales     |
 +-------+--------------------------------+-----------+
@@ -82,3 +81,23 @@ output:
 -- Promotion Effectiveness and Efficiency by Years
 
 SELECT
+	YEAR(order_date) AS years,
+	SUM(sales) AS sales,
+	SUM(discount_value) AS promotion_value,
+	ROUND((SUM(discount_value)/SUM(sales)) * 100, 2) AS burn_rate_percentage
+FROM dqlab_sales_store
+WHERE order_status = 'Order Finished'
+GROUP BY years
+
+-- output:
++-------+------------+-----------------+----------------------+
+| years | sales      | promotion_value | burn_rate_percentage |
++-------+------------+-----------------+----------------------+
+|  2009 | 4613872681 |       214330327 |                 4.65 |
+|  2010 | 4059100607 |       197506939 |                 4.87 |
+|  2011 | 4112036186 |       214611556 |                 5.22 |
+|  2012 | 4482983158 |       225867642 |                 5.04 |
++-------+------------+-----------------+----------------------+
+
+
+-- Promotion Effectiveness and Efficiency by Product Sub-category
